992ae17663008fd6c698849ad76281210fc4756b
Brayden Copley <braydenc303@gmail.com>
2019-02-02 11:32:45 -0700

    Completed Auth.js

--- client/src/pages/Auth/Auth.js
+++ client/src/pages/Auth/Auth.js
@@ -22,4 +22,81 @@ class Auth extends Component {
             [name]: value
         });
     };
-}
+
+    handleLogin = event => {
+        event.preventDefault();
+        if(this.state.username && this.state.password) {
+            API.login({
+                username: this.state.username,
+                password: this.state.password
+            }).then(user => {
+                console.log(user);
+                if(user.data.loggedIn) {
+                    this.setState({
+                        loggedIn: true,
+                        user: user.data.user
+                    });
+                    console.log("login successful");
+                    window.location.href = '/profile';
+                }
+                else if (user.data.message) {
+                    this.setState({
+                        message: user.data.message
+                    })
+                }
+            });
+        }
+    };
+
+    handleSignup = event => {
+        event.preventDefault();
+        if (this.state.username && this.state.password) {
+            API.signup({
+                username: this.state.username,
+                password: this.state.password
+            }).then(user => {
+                if(user.data.loggedIn) {
+                    this.setState({
+                        loggedIn: true,
+                        user: user.data.user
+                    });
+                    console.log("login successful");
+                    window.location.href = '/profile';
+                } else {
+                    console.log("Something went wrong:(");
+                    console.log(user.data);
+                    this.setState({
+                        message: user.data
+                    });
+                }
+            });
+        }
+    }
+
+    render(){
+        return(
+            <div className="authBox">
+                {(this.props.action === "login") ? (
+                    <Login
+                        username={this.state.username}
+                        password={this.state.password}
+                        handleLogin={this.handleLogin}
+                        handleInputChange={this.handleInputChange}
+                        message={this.state.message}
+                    />
+                ) : (
+                    <Signup
+                        username={this.state.username}
+                        password={this.state.password}
+                        confirmPassword={this.state.confirmPassword}
+                        handleSignup={this.handleSignup}
+                        handleInputChange={this.handleInputChange}
+                        message={this.state.message}
+                    />
+                )}
+            </div>
+        )
+    }
+};
+
+export default Auth;
