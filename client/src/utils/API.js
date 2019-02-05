ae3dc15738e49ea94b608188d2f267fb2de15868
AChapman <achapman77@gmail.com>
2019-02-05 11:08:21 -0700

    Setup card to game create association

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -64,7 +64,7 @@ export default {
         return axios.delete(`/api/cards/${id}`);
     },
     // Saves a card to the database
-    saveCard: function(cardData) {
+    saveCard: function(cardData, gameID) {
         return axios.post("/api/cards", cardData);
     }
 };
