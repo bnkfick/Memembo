bc88564c449c08d9e36315d82758ff86bedd6d7c
rkoven74 <roxannekoven@gmail.com>
2019-02-06 10:05:34 -0700

    Search filter now on gamelist

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { InputGroup, InputGroupAddon, Button, Input } from 'reactstrap';
 import styled from 'styled-components';
 
-const StyledInputGroup = styled(InputGroup) `
+const StyledInputGroup = styled(InputGroup)`
 max-width: 40rem;
 margin: 0 1rem;
 `
@@ -28,19 +28,51 @@ const SearchInput = styled(Input)`
     background-color: rgb(232, 216, 255);
 `
 
-const Search = () => {
+const Search = (props) => {
     return (
         <>
             <StyledInputGroup>
                 <SearchInput
+                    onChange={props.onChange}
                     placeholder="Search Games"
                 />
                 <InputGroupAddon addonType="append">
-                <SearchButton ><i className="fa fa-search" aria-hidden="true"></i></SearchButton>
+                    <SearchButton ><i className="fa fa-search" aria-hidden="true"></i></SearchButton>
                 </InputGroupAddon>
             </StyledInputGroup>
         </>
     )
 }
 
+
+              
 export default Search;
+
+
+// function Search(props) {
+//     return (
+//       <form className="search">
+//                     <div className="form-group">
+//                         <label htmlFor="breed">Breed Name:</label>
+//                         <input
+//                             value={props.search}
+//                             onChange={props.handleInputChange}
+//                             name="breed"
+//                             list="breeds"
+//                             type="text"
+//                             className="form-control"
+//                             placeholder="Type in a dog breed to begin"
+//                             id="breed"
+//                         />
+//                         <datalist id="breeds">
+//                             {props.breeds.map(breed => (
+//                                 <option value={breed} key={breed} />
+//                             ))}
+//                         </datalist>
+//                         <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+//                             Search
+//           </button>
+//                     </div>
+//                 </form>
+                
+             
--- client/src/pages/GameList/index.js
+++ client/src/pages/GameList/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
-import API from "../../utils/API"
+import API from "../../utils/API";
+import Search from "../../components/header/Search";
 
 // Exporting both GameList and GameListItem from this file
 
@@ -8,36 +9,60 @@ import API from "../../utils/API"
 class GameList extends Component {
 
     state = {
-        games: []
+        games: [],
+        query: ""
     }
 
     componentDidMount() {
         API.getGames().then(res => {
             console.log(res.data);
             this.setState({
-                games: res.data
+               games: res.data
+            
             })
         })
         .catch(err => console.log(err));
     }
 
+    onChange = event => {
+
+        this.setState({
+            query: event.target.value
+        })
+    }
+    renderGame=game => {
+        const query= this.state.query;
+        console.log(game)
+        if(query !== "" && game.gameGroup.toLowerCase().indexOf(query.toLowerCase())=== -1){
+            return null;
+        }
+
+        return <li>
+            <a href={`/play/${game._id}`}>
+            {game.gameName}
+            </a>
+
+        </li>
+    }
 
     render() {
         return (
+
+            <>
+            <Search
+                query={this.state.query}
+                onChange={this.onChange}
+
+            />
             <ul>
             { this.state.games.map(game => {
-                return (
-                <li>
-                        {/* <p>GAME: {game._id}</p> */}
-                        <a href={`/play/${game._id}`}>
-                        {game.gameName}
-                        </a>
-                </li>
-                );
+                return this.renderGame(game);
             })}
             </ul>
+            </>
         )
-    }  
-}
+    }
+}  
+
 
 export default GameList;
